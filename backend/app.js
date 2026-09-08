ea243ab721adfb1367310a364f31346920b85084
Musheeb <museebnoorisys240@gmail.com>
2026-09-09 00:50:03 +0530

    feat: integrated docker compose watch successfully

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
+require("dotenv").config({ quiet: true });
 const express = require("express");
 const app = express();
 const database = require("./config/db.config");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 app is running on PORT ${PORT || 3000}`);
+  console.log(`🚀 app is running on PORT ${PORT}`);
 });
 
 database();
--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/mern-docker-watch");
+    // await mongoose.connect("mongodb://localhost:27017/mern-docker-watch");
+    await mongoose.connect("mongodb://db:27017/mern-docker-watch");
     console.log("📚 Database connection established");
   } catch (e) {
     console.log("Database connection failed");
